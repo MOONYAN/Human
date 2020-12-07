409169310744abf02319a1cf43e1835bf2f8f74c
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-07 23:26:59 +0800

    add component human-form

--- src/app/human/human.module.ts
+++ src/app/human/human.module.ts
@@ -5,14 +5,17 @@ import { HumanRoutingModule } from './human-routing.module';
 import { HumanComponent } from './human.component';
 import { HumanListComponent } from './human-list/human-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { HumanFormComponent } from './human-form/human-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [HumanComponent, HumanListComponent],
+  declarations: [HumanComponent, HumanListComponent, HumanFormComponent],
   imports: [
     CommonModule,
     HumanRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
   ]
 })
 export class HumanModule { }
